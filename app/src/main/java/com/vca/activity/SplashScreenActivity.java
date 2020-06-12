8b783f40e7275fb42c78a71d581295cc9ecfd47a
p <p>
2020-06-12 18:20:07 +0530

    init commit
    init commit

---
+++ app/src/main/java/com/vca/activity/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.vca.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vca.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
